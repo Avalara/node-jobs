679f1a0c641e411a9ecab5b6c0f9285186fbacd2
balasuar <balasuar@gmail.com>
2014-03-26 22:33:26 -0700

    Start idle workers

--- test.js
+++ test.js
@@ -7,15 +7,21 @@ if(args[0] === '--create') {
 
 	var log = function(data) {console.log(data); };
 
-	for(var i = 0; i < 20; ++i) {
-		var job = jobs.create('job-type1',{
-			value: '123',
-			value2: '234'
-		});
+	var createJobs = function() {
+		for(var i = 0; i < 20; ++i) {
+			var job = jobs.create('job-type1',{
+				value: '123',
+				value2: '234'
+			});
 
-		job.on('update',log);
-		job.on('complete',log);
-	}
+			job.on('update',log);
+			job.on('complete',log);
+		}
+	};
+
+	createJobs();
+
+	setInterval(createJobs,5000);
 
 } else {
 	jobs.process('job-type1',2,function(job,callback) {
@@ -23,12 +29,11 @@ if(args[0] === '--create') {
 			return console.log('no job');
 		}
 
+		console.log('Processing job: ' + job.id);
 		job.update(job.id + ': this is a job update!');
-
-
 		job.complete('yay done');
-
-
-		callback(null,'someoutput','this is my result');
+		setTimeout(function() {
+			callback(null,'someoutput','this is my result');
+		},10000);
 	});
 }
